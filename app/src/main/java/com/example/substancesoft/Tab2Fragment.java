154f8e0d21b04b69da3e63d5c1bf67836ac754c5
XV02 <lghhssi@gmail.com>
2019-04-17 19:48:55 -0500

    Jaja Actualiza

--- app/src/main/java/com/example/substancesoft/Tab2Fragment.java
+++ app/src/main/java/com/example/substancesoft/Tab2Fragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -17,20 +18,18 @@ public class Tab2Fragment extends Fragment {
     private static final String TAG = "Tab2Fragment";
 
     private Button btnTEST;
+    private WebView webView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_fragment,container,false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST2);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 2",Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        webView = (WebView) view.findViewById(R.id.WebView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        final IP ip = new IP("http://" + getString(R.string.address));
+        String url = ip.getAddress()+"/substancesoft/mobile/demanda.php";
+        webView.loadUrl(url);
         return view;
     }
 }
