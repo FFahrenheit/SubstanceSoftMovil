090eaa13dc7377183e23315c3f3610489ec49c0b
XV02 <lghhssi@gmail.com>
2019-05-26 11:03:02 -0500

    De hecho que te sque es para mensos

--- app/src/main/java/com/example/substancesoft/MainScreen.java
+++ app/src/main/java/com/example/substancesoft/MainScreen.java
@@ -62,7 +62,5 @@ public class MainScreen extends AppCompatActivity
     {
         if(getFragmentManager().getBackStackEntryCount() > 0)
             getFragmentManager().popBackStack();
-        else
-            super.onBackPressed();
     }
 }
